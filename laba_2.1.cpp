2a67b601dba31730df8d334f6393cd8b661c3206
Nikita-Solokha <147943333+Nikita-Solokha@users.noreply.github.com>
2024-03-09 02:00:34 +0300

    Add files via upload

---
+++ laba_2.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int U[7];
+    int D[7];
+    int V[7];
+
+    srand(time(0));
+
+    
+    for (int i = 0; i < 7; i++) {
+        U[i] = rand() % 81 - 40; 
+        D[i] = rand() % 81 - 40; 
+        V[i] = rand() % 81 - 40; 
+    }
+
+    int* ptrU = U;
+    int* ptrD = D;
+    int* ptrV = V;
+
+    for (int i = 0; i < 7; i++) {
+        double avg_temp = (*ptrU + *ptrD + *ptrV) / 3.0;
+        std::cout << "Средняя температура за день " << i + 1 << ": " << avg_temp << std::endl;
+        ptrU++;
+        ptrD++;
+        ptrV++;
+    }
+
+    return 0;
+}
---
+++ laba_2.2.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    std::cout << "Введите количество элементов в массиве: ";
+    std::cin >> n;
+
+    double* arr = new double[n];
+    std::cout << "Введите элементы массива: ";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> *(arr + i);
+    }
+
+    // Найти номер максимального по модулю элемента массива
+    int max_abs_idx = 0;
+    double max_abs_value = std::abs(*arr);
+    double* ptr = arr;
+    for (int i = 1; i < n; ++i) {
+        if (std::abs(*(ptr + i)) > max_abs_value) {
+            max_abs_value = std::abs(*(ptr + i));
+            max_abs_idx = i;
+        }
+    }
+    std::cout << "Индекс максимального элемента по модулю: " << max_abs_idx << std::endl;
+
+    // Вычислить сумму элементов массива после первого положительного элемента
+    double sum_after_positive = 0;
+    bool found_positive = false;
+    for (int i = 0; i < n; ++i) {
+        if (found_positive) {
+            sum_after_positive += *(ptr + i);
+        }
+        else if (*(ptr + i) > 0) {
+            found_positive = true;
+        }
+    }
+    std::cout << "Сумма элементов после первого положительного элемента: " << sum_after_positive << std::endl;
+
+    // Преобразовать массив
+    double a, b;
+    std::cout << "Введите диапазон [a, b] для фильтрации целой части: ";
+    std::cin >> a >> b;
+
+    std::vector<double> filtered_arr;
+    ptr = arr;
+    for (int i = 0; i < n; ++i) {
+        if (static_cast<int>(*(ptr + i)) >= a && static_cast<int>(*(ptr + i)) <= b) {
+            filtered_arr.push_back(*(ptr + i));
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        if (static_cast<int>(*(ptr + i)) < a || static_cast<int>(*(ptr + i)) > b) {
+            filtered_arr.push_back(*(ptr + i));
+        }
+    }
+
+    std::cout << "Преобразованный массив: ";
+    for (double elem : filtered_arr) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] arr; // Освобождение памяти, выделенной для массива
+    return 0;
+}
